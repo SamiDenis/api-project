230b2ba391e744788953af50ccfa65454ad627ff
Samantha Denis <sami.m.denis@gmail.comM>
2020-10-23 18:19:35 -0400

    FIANL

---
+++ script.js
@@ -0,0 +1,28 @@
+let url1 ="http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+ let toc = document.querySelector(".toc")
+ 
+ 
+
+let images = document.querySelectorAll("img")
+let buttons = document.querySelectorAll("button")
+let paragraphs = document.querySelectorAll("p")
+
+toc.style.opacity = 0;
+
+
+for(let i=0; i<images.length; i++) {
+buttons[i].addEventListener("click", function (e){
+  e.preventDefault();
+  
+    fetch(url1)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      images[i].src = data[i].image_link
+      paragraphs[i].innerText = data[i].description
+    })
+
+    toc.style.opacity = 100;
+
+  })
+}
